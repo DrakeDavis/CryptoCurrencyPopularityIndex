4ccb0f5a30935eb8871ce2a41d0002f277966fb2
Drake <drakesaddress@gmail.com>
2020-06-07 14:04:36 -0400

    Updating logic for "trending" popularity. Now compares Current Popularity against an average of last week's popularity

--- CPI/CryptoPopularityIndex/GoogleTrendsRetriever.py
+++ CPI/CryptoPopularityIndex/GoogleTrendsRetriever.py
@@ -1,12 +1,12 @@
 from pytrends.request import TrendReq
 import time
+
 pytrends = TrendReq(hl='en-US', tz=360)
 
 
 # This function calls out to Google Trends and gets the popularity metrics
 # for the currency that is passed as an argument.
 def findTrend(argCurrency):
-
     # Keyword to search for
     keyword = argCurrency.name
 
@@ -15,40 +15,37 @@ def findTrend(argCurrency):
 
     # Build the payload, the timeframe is one month
     kw_list = [keyword]
-    pytrends.build_payload(kw_list, cat=0, timeframe='today 1-m', geo='', gprop='')
+    pytrends.build_payload(kw_list, cat=0, timeframe='now 7-d', geo='', gprop='')
 
     # Make the 'interest over time' call
     dataFrame = pytrends.interest_over_time()
 
     # Try to get a list of daily popularity over the last month
     try:
-        monthlyList = dataFrame.iloc[:, 0].tolist()
+        weeklyPopularityList = dataFrame.iloc[:, 0].tolist()
     except:
         return currency
 
-    # Creating subsets of data for week and 3day
-    weeklyList = (monthlyList[-7:])
-    threeDatList = (weeklyList[-3:])
-    lastDay = (weeklyList[-1:])
+    # The current popularity is the last index in the list of weekly popularity scores
+    currentPopularity = weeklyPopularityList[-1]
+    currency.currentPopularity = currentPopularity
+
+    weeklyPopularityList = [i for i in weeklyPopularityList if i != 0]
 
     # Getting the averages of the above lists
-    monthly_Average = float(sum(monthlyList)) / len(monthlyList)
-    weekly_Average = float(sum(weeklyList)) / len(weeklyList)
-    threeDay_Average = float(sum(threeDatList)) / len(threeDatList)
+    weekly_Average = float(sum(weeklyPopularityList)) / len(weeklyPopularityList)
 
     # Populating that data into the model object. The %.2 should truncate to 2 decimal places
-    currency.monthlyAveragePopularity = ("%.2f" % monthly_Average)
     currency.weeklyAveragePopularity = ("%.2f" % weekly_Average)
-    currency.threeDayAveragePopularity = ("%.2f" % threeDay_Average)
-    currency.lastDayPopularity = lastDay[0]
 
     # Sometimes google trends returns 0 if something goes wrong. We definitely don't want to divide by 0
-    if (monthly_Average != 0):
-        increaseBetween3dAnd30d = threeDay_Average / monthly_Average
+    if weeklyPopularityList != 0:
+        changeBetween7dAndCurrentAverage = currentPopularity / weekly_Average
     else:
-        increaseBetween3dAnd30d = 0
+        changeBetween7dAndCurrentAverage = 0
 
     # Populate the currency object with relevant data
-    currency.percentChange = float("%.2f" % (increaseBetween3dAnd30d *100))
+    currency.percentChange = float("%.2f" % (changeBetween7dAndCurrentAverage * 100))
     currency.lastUpdated = str(int(time.time()))
+
     return currency
